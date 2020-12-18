13d08a0fceb719cc45d45e8a6ff9981764724f60
Colton Loewen <loewencjay@outlook.com>
2020-12-17 22:31:38 -0700

    Minor ItemEntity mixin improvements

--- src/main/java/ca/cjloewen/corntopia/entity/CornItemEntity.java
+++ src/main/java/ca/cjloewen/corntopia/entity/CornItemEntity.java
@@ -1,10 +1,16 @@
 package ca.cjloewen.corntopia.entity;
 
-import ca.cjloewen.corntopia.mixin.CopyableItemEntityMixin;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import ca.cjloewen.corntopia.mixin.ItemEntityMixin;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.damage.DamageSource;
 
 public class CornItemEntity extends ItemEntity {
+	private static final Logger LOGGER = LogManager.getLogger();
+	
 	public CornItemEntity(ItemEntity originalEntity) {
 		super(EntityType.ITEM, originalEntity.world);
 		setStack(originalEntity.getStack());
@@ -12,7 +18,7 @@ public class CornItemEntity extends ItemEntity {
 		refreshPositionAndAngles(originalEntity.getX(), originalEntity.getY(), originalEntity.getZ(), originalEntity.getYaw(1f), originalEntity.getPitch(1f));
 		setVelocity(originalEntity.getVelocity());
 		setEntityId(originalEntity.getEntityId());
-		setPickupDelay(((CopyableItemEntityMixin)originalEntity).getPickupDelay());
+		setPickupDelay(((ItemEntityMixin)originalEntity).getPickupDelay());
 		setUuid(originalEntity.getUuid());
 	}
 }
--- src/main/java/ca/cjloewen/corntopia/mixin/ItemEntityMixin.java
+++ src/main/java/ca/cjloewen/corntopia/mixin/ItemEntityMixin.java
@@ -6,7 +6,13 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 import net.minecraft.entity.ItemEntity;
 
 @Mixin(ItemEntity.class)
-public abstract interface CopyableItemEntityMixin {
+public abstract interface ItemEntityMixin {
 	@Accessor("pickupDelay")
 	public int getPickupDelay();
+	
+	@Accessor("health")
+	public int getHealth();
+	
+	@Accessor("health")
+	public void setHealth(int health);
 }
